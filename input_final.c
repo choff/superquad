a3652f5200e1f3c91279d59ddc911dee078cf305
Christoph Wurm <christoph.wurm@de.ibm.com>
2011-05-16 08:54:37 +0200

    input.c for acceptance

---
+++ input_final.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+
+// This is a demo programm
+
+
+float function (int x, int y);
+
+int main (void){
+	
+	/* as you can see, 
+	this is a multi line
+	comment */
+	
+	int _variable1;
+	int _variable2;
+	int _variable3;
+	
+	_variable2 = 2;
+	_variable3 = 5;
+	
+	_variable1 =  32 - _variable3 *5;
+	
+	
+	
+	do{
+	/*this loop runs how many times?*/			
+		_variable2 = _variable2 << 2;
+		_variable1 = _variable1 + 1;
+	}while(_variable1 <10 );
+	
+	// what is the value of _variable2 "Hab ...!"	
+	
+	return 1;
+	
+}
+
+
+
+
+
